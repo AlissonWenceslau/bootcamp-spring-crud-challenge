f1ed64b6124cbbd04e2067c383f9a5a4a6508533
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-01-19 19:40:14 -0300

    Transactional annotation

--- src/main/java/com/alisson/challenge/first/chapter/client/services/ClientService.java
+++ src/main/java/com/alisson/challenge/first/chapter/client/services/ClientService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alisson.challenge.first.chapter.client.dto.ClientDTO;
 import com.alisson.challenge.first.chapter.client.entities.Client;
@@ -15,6 +16,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(PageRequest pageRequest){
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
